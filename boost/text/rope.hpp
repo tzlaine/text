9026c405eecd847e8d3a0c7305f519808a3fb12b
Zach Laine <whatwasthataddress@gmail.com>
2017-06-15 22:02:05 -0500

    Add initial sketch of rope_view.

--- boost/text/rope.hpp
+++ boost/text/rope.hpp
@@ -304,32 +304,130 @@ namespace boost { namespace text {
         friend struct detail::const_rope_iterator;
     };
 
+    struct rope_view
+    {
+        using iterator = rope::iterator;
+        using const_iterator = rope::const_iterator;
+        using reverse_iterator = rope::reverse_iterator;
+        using const_reverse_iterator = rope::const_reverse_iterator;
+
+        using size_type = std::ptrdiff_t;
+
+        rope_view () noexcept : r_ (nullptr), lo_ (0), hi_ (0) {}
+        rope_view (rope const & r) noexcept : r_ (&r), lo_ (0), hi_ (r.size()) {}
+
+        rope_view (rope const & r, int lo, int hi);
+
+        rope_view (rope const & r, int lo, int hi, utf8::unchecked_t) noexcept :
+            r_ (&r), lo_ (lo), hi_ (hi)
+        {}
+
+        rope_view (rope_view const & rhs) noexcept :
+            r_ (rhs.r_), lo_ (rhs.lo_), hi_ (rhs.hi_)
+        {}
+
+        rope_view & operator= (rope_view const & rhs) noexcept
+        {
+            r_ = rhs.r_;
+            lo_ = rhs.lo_;
+            hi_ = rhs.hi_;
+            return *this;
+        }
+
+        const_iterator begin () const noexcept;
+        const_iterator end () const noexcept;
+
+        const_reverse_iterator rbegin () const noexcept;
+        const_reverse_iterator rend () const noexcept;
+
+        bool empty () const noexcept
+        { return lo_ == hi_; }
+
+        size_type size () const noexcept
+        { return hi_ - lo_; }
+
+        char operator[] (int i) const noexcept
+        {
+            assert(lo_ + i < r_->size());
+            return (*r_)[lo_ + i];
+        }
+
+        rope_view operator() (int lo, int hi) const
+        {
+            if (lo < 0)
+                lo += size();
+            if (hi < 0)
+                hi += size();
+            assert(0 <= lo && lo <= size());
+            assert(0 <= hi && hi <= size());
+            assert(lo <= hi);
+            return rope_view(*r_, lo_ + lo, lo_ + hi);
+        }
+
+        rope_view operator() (int cut) const
+        {
+            int lo = 0;
+            int hi = cut;
+            if (cut < 0) {
+                lo = cut + size();
+                hi = size();
+            }
+            assert(0 <= lo && lo <= size());
+            assert(0 <= hi && hi <= size());
+            return rope_view(*r_, lo_ + lo, lo_ + hi);
+        }
+
+        constexpr size_type max_size () const noexcept
+        { return PTRDIFF_MAX; }
+
+        int compare (rope_view rhs) const noexcept;
+
+        friend bool operator== (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) == 0; }
+
+        friend bool operator!= (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) != 0; }
+
+        friend bool operator< (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) < 0; }
+
+        friend bool operator<= (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) <= 0; }
+
+        friend bool operator> (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) > 0; }
+
+        friend bool operator>= (rope_view lhs, rope_view rhs) noexcept
+        { return lhs.compare(rhs) >= 0; }
+
+        void swap (rope_view & rhs) noexcept
+        {
+            std::swap(r_, rhs.r_);
+            std::swap(lo_, rhs.lo_);
+            std::swap(hi_, rhs.hi_);
+        }
+
+#if 0 // TODO
+        friend std::ostream & operator<< (std::ostream & os, rope_view view)
+        { return os.write(view.begin(), view.size()); }
+#endif
+
+    private:
+        rope const * r_;
+        int lo_;
+        int hi_;
+    };
+
 } }
 
 #include <boost/text/detail/rope_iterator.hpp>
 
 namespace boost { namespace text {
 
+    // rope
+
     int rope::compare (rope rhs) const noexcept
-    {
-        // TODO: This could probably be optimized quite a bit by doing
-        // something equivalent to mismatch, segment-wise.
-        auto const iters = std::mismatch(begin(), end(), rhs.begin(), rhs.end());
-        if (iters.first == end()) {
-            if (iters.second == rhs.end())
-                return 0;
-            else
-                return -1;
-        } else if (iters.second == rhs.end()) {
-            return 1;
-        } else if (*iters.first == *iters.second) {
-            return 0;
-        } else if (*iters.first < *iters.second) {
-            return -1;
-        } else {
-            return 1;
-        }
-    }
+    { return rope_view(*this).compare(rhs); }
 
     rope::const_iterator rope::begin () const noexcept
     { return const_iterator(*this, 0); }
@@ -350,6 +448,59 @@ namespace boost { namespace text {
     { return r.rbegin(); }
     rope::const_reverse_iterator rend (rope const & r) noexcept
     { return r.rend(); }
+
+
+    // rope_view
+
+    inline rope_view::rope_view (rope const & r, int lo, int hi) :
+        r_ (&r), lo_ (lo), hi_ (hi)
+    {
+        if (!utf8::starts_encoded(begin(), end()))
+            throw std::invalid_argument("The start of the given string is not valid UTF-8.");
+        if (!utf8::ends_encoded(begin(), end()))
+            throw std::invalid_argument("The end of the given string is not valid UTF-8.");
+    }
+
+    inline rope_view::const_iterator rope_view::begin () const noexcept
+    { return r_->begin() + lo_; }
+    inline rope_view::const_iterator rope_view::end () const noexcept
+    { return r_->begin() + hi_; }
+
+    inline rope_view::const_reverse_iterator rope_view::rbegin () const noexcept
+    { return const_reverse_iterator(r_->begin() + hi_ - 1); }
+    inline rope_view::const_reverse_iterator rope_view::rend () const noexcept
+    { return const_reverse_iterator(r_->begin() + lo_ - 1); }
+
+    inline int rope_view::compare (rope_view rhs) const noexcept
+    {
+        // TODO: This could probably be optimized quite a bit by doing
+        // something equivalent to mismatch, segment-wise.
+        auto const iters = std::mismatch(begin(), end(), rhs.begin(), rhs.end());
+        if (iters.first == end()) {
+            if (iters.second == rhs.end())
+                return 0;
+            else
+                return -1;
+        } else if (iters.second == rhs.end()) {
+            return 1;
+        } else if (*iters.first == *iters.second) {
+            return 0;
+        } else if (*iters.first < *iters.second) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    inline rope_view::iterator begin (rope_view v) noexcept
+    { return v.begin(); }
+    inline rope_view::iterator end (rope_view v) noexcept
+    { return v.end(); }
+
+    inline rope_view::reverse_iterator rbegin (rope_view v) noexcept
+    { return v.rbegin(); }
+    inline rope_view::reverse_iterator rend (rope_view v) noexcept
+    { return v.rend(); }
 
 } }
 
