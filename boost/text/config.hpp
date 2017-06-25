da6eca95a0863b8e66009008d93056ff72d07380
Zach Laine <whatwasthataddress@gmail.com>
2017-06-25 17:04:54 -0500

    Fix special-casing of GCC 6 vs. earlier.

--- boost/text/config.hpp
+++ boost/text/config.hpp
@@ -4,13 +4,10 @@
 #include <boost/config.hpp>
 
 
-#if defined(__GNUC__)
-# if 6 <= __GNUC__
-#  define BOOST_TEXT_CXX14_CONSTEXPR BOOST_CXX14_CONSTEXPR
-# else // Nothing before GCC 6 has proper C++14 constexpr support.
-#  define BOOST_TEXT_CXX14_CONSTEXPR
-#  define BOOST_TEXT_NO_CXX14_CONSTEXPR
-# endif
+// Nothing before GCC 6 has proper C++14 constexpr support.
+#if defined(__GNUC__) && __GNUC__ < 6
+# define BOOST_TEXT_CXX14_CONSTEXPR
+# define BOOST_TEXT_NO_CXX14_CONSTEXPR
 #else
 # define BOOST_TEXT_CXX14_CONSTEXPR BOOST_CXX14_CONSTEXPR
 # if defined(BOOST_NO_CXX14_CONSTEXPR)
