4ee5ec024e0dce2a789d4dd1f2de695086a63879
Zach Laine <whatwasthataddress@gmail.com>
2017-06-28 20:09:37 -0500

    Remove a rethought TODO.

--- boost/text/rope.hpp
+++ boost/text/rope.hpp
@@ -18,9 +18,6 @@ namespace boost { namespace text {
         struct const_reverse_rope_iterator;
     }
 
-    // TODO: Now that rope_view covers rope and text, consider dumping rope's
-    // and text's compare() and boolean comparisons.
-
     /** A mutable sequence of char with copy-on-write semantics.  The sequence
         is assumed to be UTF-8 encoded, though it is possible to construct a
         sequence which is not. A rope is non-contiguous and is not
