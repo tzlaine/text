df4ad94ba1dfa7b44783459ba33ef012b91a7620
Zach Laine <whatwasthataddress@gmail.com>
2017-07-06 21:19:59 -0500

    Add optional rope threadsafety test using TSan.

---
+++ test/rope_threadsafety.cpp
@@ -0,0 +1,43 @@
+#include <boost/text/rope.hpp>
+
+#include <atomic>
+#include <thread>
+
+
+std::atomic<bool> done(false);
+
+void thread_function (boost::text::rope const & r)
+{
+    boost::text::rope local_r = r;
+
+    while (!done)
+        ;
+}
+
+int main ()
+{
+    std::unique_ptr<boost::text::rope> r(new boost::text::rope("some text"));
+    
+    std::thread thread_0(thread_function, *r);
+    std::thread thread_1(thread_function, *r);
+    std::thread thread_2(thread_function, *r);
+    std::thread thread_3(thread_function, *r);
+
+    std::thread thread_4(thread_function, *r);
+    std::thread thread_5(thread_function, *r);
+    std::thread thread_6(thread_function, *r);
+    std::thread thread_7(thread_function, *r);
+
+    r.release();
+    done = true;
+
+    thread_0.join();
+    thread_1.join();
+    thread_2.join();
+    thread_3.join();
+
+    thread_4.join();
+    thread_5.join();
+    thread_6.join();
+    thread_7.join();
+}
