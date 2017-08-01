0377d34fc9438b6181932f74a33e3a4f934defda
Zach Laine <whatwasthataddress@gmail.com>
2017-07-31 22:17:37 -0500

    Round out ctor/dtor perf tests.

--- perf/ctor_dtor_perf.cpp
+++ perf/ctor_dtor_perf.cpp
@@ -1,34 +1,11 @@
 #include <boost/text/text.hpp>
 
+#include "strings.hpp"
+
 #include <benchmark/benchmark.h>
 
 #include <iostream>
-#include <string>
 
-
-namespace {
-
-    std::string make_string (int size)
-    { return std::string(size, '.'); }
-
-    std::string strings[14] = {
-        make_string(0),
-        make_string(1 << 0),
-        make_string(1 << 1),
-        make_string(1 << 2),
-        make_string(1 << 3),
-        make_string(1 << 4),
-        make_string(1 << 5),
-        make_string(1 << 6),
-        make_string(1 << 8),
-        make_string(1 << 10),
-        make_string(1 << 12),
-        make_string(1 << 16),
-        make_string(1 << 20),
-        make_string(1 << 30)
-    };
-
-}
 
 void BM_text_view_ctor_dtor (benchmark::State & state)
 {
@@ -55,20 +32,40 @@ void BM_text_view_ctor_dtor_unchecked (benchmark::State & state)
     }
 }
 
-#define ALL_BENCHMARK_ARGS()                    \
-    ->Arg(0)->Arg(1)->Arg(2)->Arg(3)->Arg(4)    \
-    ->Arg(5)->Arg(6)->Arg(7)->Arg(8)->Arg(9)    \
-    ->Arg(10)->Arg(11)->Arg(12)->Arg(13)
+void BM_text_ctor_dtor (benchmark::State & state)
+{
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(
+            boost::text::text(strings[state.range(0)])
+        );
+    }
+}
 
-#ifdef FULL_PERF_TESTS
-#define BENCHMARK_ARGS() ALL_BENCHMARK_ARGS()
-#else
-#define BENCHMARK_ARGS()                        \
-    ->Arg(0)->Arg(1)->Arg(2)->Arg(3)->Arg(4)    \
-    ->Arg(5)->Arg(6)->Arg(7)->Arg(8)->Arg(9)
-#endif
+void BM_rope_ctor_dtor (benchmark::State & state)
+{
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(
+            boost::text::rope(boost::text::text_view(strings[state.range(0)]))
+        );
+    }
+}
 
-BENCHMARK(BM_text_view_ctor_dtor) ALL_BENCHMARK_ARGS();
-BENCHMARK(BM_text_view_ctor_dtor_unchecked) ALL_BENCHMARK_ARGS();
+void BM_rope_view_ctor_dtor (benchmark::State & state)
+{
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(
+            boost::text::rope_view(boost::text::text_view(strings[state.range(0)]))
+        );
+    }
+}
+
+BENCHMARK(BM_text_view_ctor_dtor) BENCHMARK_ARGS();
+BENCHMARK(BM_text_view_ctor_dtor_unchecked) BENCHMARK_ARGS();
+
+BENCHMARK(BM_text_ctor_dtor) BENCHMARK_ARGS();
+
+BENCHMARK(BM_rope_ctor_dtor) BENCHMARK_ARGS();
+
+BENCHMARK(BM_rope_view_ctor_dtor) BENCHMARK_ARGS();
 
 BENCHMARK_MAIN()
