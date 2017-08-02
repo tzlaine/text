e41f3912e8f2a63a36cd8282eeb3145e970788ba
Zach Laine <whatwasthataddress@gmail.com>
2017-08-01 23:02:04 -0500

    Add perf_{snapshot,comparison} targets; check in the first perf snapshot.

---
+++ perf/snapshot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import shutil
+import subprocess
+
+
+parser = argparse.ArgumentParser(description='Create a snapshot of .json files from Google Benchmark.')
+parser.add_argument('--output-root', '-o', type=str, required=True,
+                    help='the root of the tree into which to write the json files')
+args = parser.parse_args()
+
+
+git_log = subprocess.check_output(['git', 'log', '-1'])
+commit = git_log.splitlines()[0][7:]
+
+out_dir = os.path.abspath(args.output_root)
+
+latest_dir = os.path.join(out_dir, 'latest_snapshot')
+out_dir = os.path.join(out_dir, 'snapshots', commit)
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+def copy_file(filename):
+    shutil.copyfile(filename, os.path.join(latest_dir, filename))
+    shutil.copyfile(filename, os.path.join(out_dir, filename))
+
+copy_file('ctor_dtor_perf.json')
+copy_file('copy_perf.json')
+copy_file('insert_erase_perf.json')
+copy_file('for_find_perf.json')
+copy_file('compare_boyer_moore_perf.json')
