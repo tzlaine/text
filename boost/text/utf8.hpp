d98565f9b2514ff3f75f317d72a213922114752c
Zach Laine <whatwasthataddress@gmail.com>
2017-08-31 20:21:25 -0500

    Grooming.

--- boost/text/utf8.hpp
+++ boost/text/utf8.hpp
@@ -958,17 +958,17 @@ namespace boost { namespace text { namespace utf8 {
                 Unicode 9, 3.9/D92
                 Table 3-7. Well-Formed UTF-8 Byte Sequences
 
-                Code Points          First Byte  Second Byte  Third Byte  Fourth Byte
-                ===========          ==========  ===========  ==========  ===========
-                U+0000..U+007F       00..7F                               
-                U+0080..U+07FF       C2..DF      80..BF                   
-                U+0800..U+0FFF       E0          A0..BF       80..BF      
-                U+1000..U+CFFF       E1..EC      80..BF       80..BF      
-                U+D000..U+D7FF       ED          80..9F       80..BF      
-                U+E000..U+FFFF       EE..EF      80..BF       80..BF      
-                U+10000..U+3FFFF     F0          90..BF       80..BF      80..BF
-                U+40000..U+FFFFF     F1..F3      80..BF       80..BF      80..BF
-                U+100000..U+10FFFF   F4          80..8F       80..BF      80..BF
+                Code Points        First Byte Second Byte Third Byte Fourth Byte
+                ===========        ========== =========== ========== ===========
+                U+0000..U+007F     00..7F
+                U+0080..U+07FF     C2..DF     80..BF
+                U+0800..U+0FFF     E0         A0..BF      80..BF
+                U+1000..U+CFFF     E1..EC     80..BF      80..BF
+                U+D000..U+D7FF     ED         80..9F      80..BF
+                U+E000..U+FFFF     EE..EF     80..BF      80..BF
+                U+10000..U+3FFFF   F0         90..BF      80..BF     80..BF
+                U+40000..U+FFFFF   F1..F3     80..BF      80..BF     80..BF
+                U+100000..U+10FFFF F4         80..8F      80..BF     80..BF
             */
 
             next_ = it_;
