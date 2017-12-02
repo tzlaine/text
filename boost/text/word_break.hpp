f6b55edeac4613c0aadf3dea1a366c546554e0af
Zach Laine <whatwasthataddress@gmail.com>
2017-12-02 11:25:37 -0600

    Add word_break TU with partially generated implementation.

---
+++ boost/text/word_break.hpp
@@ -0,0 +1,113 @@
+#ifndef BOOST_TEXT_WORD_BREAK_HPP
+#define BOOST_TEXT_WORD_BREAK_HPP
+
+#include <array>
+
+#include <stdint.h>
+
+namespace boost { namespace text {
+
+    enum class word_prop_t {
+        Other,
+        CR,
+        LF,
+        Newline,
+        Katakana,
+        ALetter,
+        MidLetter,
+        MidNum,
+        MidNumLet,
+        Numeric,
+        ExtendNumLet,
+        Regional_Indicator,
+        Hebrew_Letter,
+        Double_Quote,
+        Single_Quote,
+        E_Base,
+        E_Modifier,
+        Glue_After_Zwj,
+        E_Base_GAZ,
+        Format,
+        Extend,
+        ZWJ,
+
+        ALetter_Format,
+        ALetter_MidLetter,
+        ALetter_Single_Quote,
+        ALetter_Single_Quote_Format,
+        ALetter_MidNum,
+        Numeric_MidLetter,
+        Numeric_Single_Quote,
+        Numeric_MidNum,
+        Numeric_MidNumLet_Format
+    };
+
+    struct word_break_t
+    {
+        operator bool() const { return break_; }
+
+        bool break_ = false;
+        word_prop_t prop_ = word_prop_t::Other;
+    };
+
+    word_prop_t word_prop(uint32_t cp);
+
+    namespace detail {
+// See chart at http://www.unicode.org/Public/UCD/latest/ucd/auxiliary/WordBreakTest.html.
+constexpr std::array<std::array<bool, 22>, 31> g_word_breaks = {{
+//  Other CR LF NL Ktk AL ML MN MNL Num ENL RI HL DQ SQ E_Bse E_Mod GAZ EBG Fmt Extd ZWJ
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Other
+    {1,   1, 0, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  1,  1,   1}, // CR
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  1,  1,   1}, // LF
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  1,  1,   1}, // Newline
+    {1,   1, 1, 1, 0,  1, 1, 1, 1,  1,  0,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Katakana
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  0,  0,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // MidLetter
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // MidNum
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // MidNumLet
+                                                                                     
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  0,  0,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Numeric
+    {1,   1, 1, 1, 0,  0, 1, 1, 1,  0,  0,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ExtendNumLet
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  0, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // RI
+                                                                                     
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  0,  0,  1, 0, 1, 0, 1,    1,    1,  1,  0,  0,   0}, // Hebrew_Letter
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Double_Quote
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Single_Quote
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    0,    1,  1,  0,  0,   0}, // E_Base
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // E_Modifier
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Glue_After_Zwj
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    0,    1,  1,  0,  0,   0}, // EBG
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Format
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Extend
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    0,  0,  0,  0,   0}, // ZWJ
+                                                                                     
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  0,  0,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter_Format_FE
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  1,  1,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter_MidLetter
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  1,  1,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter_Single_Quote
+                                                                                     
+    {1,   1, 1, 1, 1,  0, 1, 1, 1,  1,  1,  1, 0, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter_Single_Quote_Format_FE
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // ALetter_MidNum
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  1,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Numeric_MidLetter
+                                                                                     
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  0,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Numeric_Single_Quote
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  0,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Numeric_MidNum
+    {1,   1, 1, 1, 1,  1, 1, 1, 1,  0,  1,  1, 1, 1, 1, 1,    1,    1,  1,  0,  0,   0}, // Numeric_MidNumLet_Format_FE
+}};
+    }
+
+    inline word_break_t word_break(word_prop_t prop, uint32_t cp)
+    {
+        auto const prop_int = static_cast<int>(prop);
+        auto const cp_prop = word_prop(cp);
+        auto const cp_prop_int = static_cast<int>(cp_prop);
+        return {detail::g_word_breaks[prop_int][cp_prop_int], cp_prop};
+    }
+
+}}
+
+#endif
