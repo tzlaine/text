01d5040131f13d5edf2dfed3e203ccea2e86e858
Zach Laine <whatwasthataddress@gmail.com>
2017-12-16 21:17:13 -0600

    Remove vestiges of static_vector from normalization_data.hpp.

--- boost/text/normalization_data.hpp
+++ boost/text/normalization_data.hpp
@@ -1,8 +1,6 @@
 #ifndef BOOST_TEXT_NORMALIZATION_DATA_HPP
 #define BOOST_TEXT_NORMALIZATION_DATA_HPP
 
-#include <boost/container/static_vector.hpp> // TODO
-
 #include <array>
 
 
@@ -29,7 +27,17 @@ namespace boost { namespace text {
         See http://www.unicode.org/reports/tr44/#Character_Decomposition_Mappings
         for the source of the "18".
     */
-    using compatability_decomposition = container::static_vector<uint32_t, 18>;
+    struct compatible_decomposition
+    {
+        using storage_type = std::array<uint32_t, 18>;
+        using iterator = storage_type::const_iterator;
+
+        iterator begin() const { return storage_.begin(); }
+        iterator end() const { return storage_.begin() + size_; }
+
+        storage_type storage_;
+        int size_;
+    };
 
     /** Returns the Canonical Combining Class for code point cp. */
     int ccc(uint32_t cp) noexcept;
