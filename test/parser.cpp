e7d29da0bb92c54e240cebb77caca6787df9a23b
Zach Laine <whatwasthataddress@gmail.com>
2018-01-06 12:49:23 -0600

    Add parse tests for options.

--- test/parser.cpp
+++ test/parser.cpp
@@ -7,7 +7,7 @@
 
 using namespace boost;
 
-TEST(parser, failures)
+TEST(parser, exceptions)
 {
     text::detail::collation_tailoring_interface callbacks = {
         [](text::detail::cp_seq_t const & reset_, bool before_) {},
@@ -16,7 +16,7 @@ TEST(parser, failures)
         [](text::variable_weighting weighting) {},
         [](text::l2_weight_order order) {},
         [](text::detail::cp_seq_t const & suppressions) {},
-        [](std::vector<text::string> &&) {},
+        [](std::vector<text::string> && reorderings) {},
         [](text::string const & s) { std::cout << s; },
         [](text::string const & s) { std::cout << s; }};
 
@@ -435,5 +435,142 @@ TEST(parser, failures)
             text::detail::parse(
                 sv.begin(), sv.end(), callbacks, "<test-string>"),
             text::parse_error);
+    }
+}
+
+TEST(parser, options)
+{
+    {
+        text::detail::cp_seq_t result;
+        text::detail::cp_seq_t const expected = {'a', 'b', 'c'};
+        text::detail::collation_tailoring_interface callbacks = {
+            [](text::detail::cp_seq_t const & reset_, bool before_) {},
+            [](text::detail::relation_t const & rel) {},
+            [](text::collation_strength strength) {},
+            [](text::variable_weighting weighting) {},
+            [](text::l2_weight_order order) {},
+            [&result](text::detail::cp_seq_t const & suppressions) {
+                result = suppressions;
+            },
+            [](std::vector<text::string> && reorderings) {},
+            [](text::string const & s) { std::cout << s; },
+            [](text::string const & s) { std::cout << s; }};
+
+        text::string_view sv = "[suppressContractions [abc]]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, expected);
+    }
+
+    {
+        text::collation_strength result;
+        text::detail::collation_tailoring_interface callbacks = {
+            [](text::detail::cp_seq_t const & reset_, bool before_) {},
+            [](text::detail::relation_t const & rel) {},
+            [&result](text::collation_strength strength) { result = strength; },
+            [](text::variable_weighting weighting) {},
+            [](text::l2_weight_order order) {},
+            [](text::detail::cp_seq_t const & suppressions) {},
+            [](std::vector<text::string> && reorderings) {},
+            [](text::string const & s) { std::cout << s; },
+            [](text::string const & s) { std::cout << s; }};
+
+        text::string_view sv;
+
+        sv = "[strength 1]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::collation_strength::primary);
+
+        sv = "[strength 2]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::collation_strength::secondary);
+
+        sv = "[strength 3]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::collation_strength::tertiary);
+
+        sv = "[strength 4]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::collation_strength::quaternary);
+
+        sv = "[strength I]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::collation_strength::identical);
+    }
+
+    {
+        text::variable_weighting result;
+        text::detail::collation_tailoring_interface callbacks = {
+            [](text::detail::cp_seq_t const & reset_, bool before_) {},
+            [](text::detail::relation_t const & rel) {},
+            [](text::collation_strength strength) {},
+            [&result](text::variable_weighting weighting) {
+                result = weighting;
+            },
+            [](text::l2_weight_order order) {},
+            [](text::detail::cp_seq_t const & suppressions) {},
+            [](std::vector<text::string> && reorderings) {},
+            [](text::string const & s) { std::cout << s; },
+            [](text::string const & s) { std::cout << s; }};
+
+        text::string_view sv;
+
+        sv = "[alternate non-ignorable]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::variable_weighting::non_ignorable);
+
+        sv = "[alternate shifted]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::variable_weighting::shifted);
+    }
+
+    {
+        text::l2_weight_order result;
+        text::detail::collation_tailoring_interface callbacks = {
+            [](text::detail::cp_seq_t const & reset_, bool before_) {},
+            [](text::detail::relation_t const & rel) {},
+            [](text::collation_strength strength) {},
+            [](text::variable_weighting weighting) {},
+            [&result](text::l2_weight_order order) { result = order; },
+            [](text::detail::cp_seq_t const & suppressions) {},
+            [](std::vector<text::string> && reorderings) {},
+            [](text::string const & s) { std::cout << s; },
+            [](text::string const & s) { std::cout << s; }};
+
+        text::string_view sv;
+
+        sv = "[backwards 2]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, text::l2_weight_order::backward);
+    }
+
+    {
+        std::vector<text::string> result;
+        std::vector<text::string> const expected = {"foo", "others", "bar"};
+        text::detail::collation_tailoring_interface callbacks = {
+            [](text::detail::cp_seq_t const & reset_, bool before_) {},
+            [](text::detail::relation_t const & rel) {},
+            [](text::collation_strength strength) {},
+            [](text::variable_weighting weighting) {},
+            [](text::l2_weight_order order) {},
+            [](text::detail::cp_seq_t const & suppressions) {},
+            [&result](std::vector<text::string> && reorderings) {
+                result = std::move(reorderings);
+            },
+            [](text::string const & s) { std::cout << s; },
+            [](text::string const & s) { std::cout << s; }};
+
+        text::string_view sv = "[reorder foo others bar]";
+        EXPECT_NO_THROW(text::detail::parse(
+            sv.begin(), sv.end(), callbacks, "<test-string>"));
+        EXPECT_EQ(result, expected);
     }
 }
