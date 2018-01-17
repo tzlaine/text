f9f8bbaa895e9c7e8805489be4711f903354d580
Zach Laine <whatwasthataddress@gmail.com>
2018-01-16 20:15:28 -0600

    Finish support for erase operations in trie fuzzer.

--- test/fuzz_trie.cpp
+++ test/fuzz_trie.cpp
@@ -21,40 +21,19 @@ namespace {
 struct action_t
 {
     enum op_t : uint8_t {
-        insert = 'a',
-//        erase = 'b',
+        insert = 0,
+        erase = 1,
 
-        num_ops
+        num_ops,
+        first_op = insert
     };
 
-    op_t op_;
     int value_;
+    op_t op_ : 1;
 };
 
-void execute(action_t action, boost::text::string_view key)
+void check()
 {
-    switch (action.op_) {
-    case action_t::insert:
-        ofs << "trie.insert("
-            << std::quoted(std::string(key.begin(), key.end())) << ", "
-            << action.value_ << "); // key.size()=" << key.size() << "\n"
-            << std::flush;
-        trie.insert(key, action.value_);
-        map.insert(std::pair<boost::text::string, int>(
-            boost::text::string(key), action.value_));
-        break;
-#if 0
-    case action_t::erase:
-        ofs << "trie.erase(" << std::quoted(std::string(key.begin(), key.end()))
-            << "); // key.size()=" << key.size() << "\n"
-            << std::flush;
-        trie.erase(key);
-        map.erase(boost::text::string(key));
-        break;
-#endif
-    default: break;
-    }
-
     assert(trie.size() == map.size());
 
     {
@@ -110,6 +89,31 @@ void execute(action_t action, boost::text::string_view key)
     }
 }
 
+void insert(boost::text::string_view key, int value)
+{
+    ofs << "trie.insert(" << std::quoted(std::string(key.begin(), key.end()))
+        << ", " << value << "); // key.size()=" << key.size() << "\n"
+        << std::flush;
+    trie.insert(key, value);
+    map.insert(
+        std::pair<boost::text::string, int>(boost::text::string(key), value));
+
+    check();
+}
+
+void erase(int which)
+{
+    auto const it = std::next(trie.begin(), which);
+    auto const & key = it->key;
+    ofs << "trie.erase(" << std::quoted(std::string(key.begin(), key.end()))
+        << "); // key.size()=" << key.size() << "\n"
+        << std::flush;
+    trie.erase(key);
+    map.erase(boost::text::string(key));
+
+    check();
+}
+
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const * data, size_t size)
 {
     if (sizeof(action_t) <= size) {
@@ -117,13 +121,22 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const * data, size_t size)
         std::memcpy(&action, data, sizeof(action_t));
         data += sizeof(action_t);
         size -= sizeof(action_t);
-        if (0 <= action.op_ && action.op_ < action_t::num_ops &&
-            size < INT_MAX) {
-            auto str = boost::text::string_view((char const *)data, size);
-            if (std::all_of(str.begin(), str.end(), [](char c) {
-                    return std::isprint(c);
-                })) {
-                execute(action, str);
+        if (action_t::first_op <= action.op_ &&
+            action.op_ < action_t::num_ops && size < INT_MAX) {
+            if (action.op_ == action_t::erase) {
+                if (!trie.empty()) {
+                    std::size_t const index =
+                        std::size_t(std::abs(action.value_)) % trie.size();
+                    assert(index < trie.size());
+                    erase(index);
+                }
+            } else {
+                auto str = boost::text::string_view((char const *)data, size);
+                if (std::all_of(str.begin(), str.end(), [](char c) {
+                        return std::isprint(c);
+                    })) {
+                    insert(str, action.value_);
+                }
             }
         }
     }
