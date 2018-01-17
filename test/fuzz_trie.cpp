5f9626a46f323e4b3607b97ed50e185ef11725c4
Zach Laine <whatwasthataddress@gmail.com>
2018-01-16 19:13:19 -0600

    Flesh out trie fuzz test a bit more; still insert-only.

--- test/fuzz_trie.cpp
+++ test/fuzz_trie.cpp
@@ -3,6 +3,7 @@
 
 #include <boost/text/trie.hpp>
 
+#include <algorithm>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -13,14 +14,15 @@ namespace {
 
     boost::trie::trie<boost::text::string, int> trie;
     std::map<boost::text::string, int> map;
-    std::ofstream ofs("inserts.cpp");
+
+    std::ofstream ofs("fuzz_operations.cpp");
 }
 
 struct action_t
 {
-    enum op_t {
-        insert,
-//        erase,
+    enum op_t : uint8_t {
+        insert = 'a',
+//        erase = 'b',
 
         num_ops
     };
@@ -33,16 +35,19 @@ void execute(action_t action, boost::text::string_view key)
 {
     switch (action.op_) {
     case action_t::insert:
-#if 0
         ofs << "trie.insert("
             << std::quoted(std::string(key.begin(), key.end())) << ", "
-            << action.value_ << "); // key.size()=" << key.size() << "\n";
-#endif
+            << action.value_ << "); // key.size()=" << key.size() << "\n"
+            << std::flush;
         trie.insert(key, action.value_);
-        map[boost::text::string(key)] = action.value_;
+        map.insert(std::pair<boost::text::string, int>(
+            boost::text::string(key), action.value_));
         break;
 #if 0
     case action_t::erase:
+        ofs << "trie.erase(" << std::quoted(std::string(key.begin(), key.end()))
+            << "); // key.size()=" << key.size() << "\n"
+            << std::flush;
         trie.erase(key);
         map.erase(boost::text::string(key));
         break;
@@ -52,29 +57,57 @@ void execute(action_t action, boost::text::string_view key)
 
     assert(trie.size() == map.size());
 
-    auto trie_it = trie.begin();
-    auto const trie_end = trie.end();
-    auto map_it = map.begin();
-    for (; trie_it != trie_end; ++trie_it, ++map_it) {
-        if (trie_it->key != map_it->first) {
-            std::cout << trie_it->key.size() << "\n"
-                      << trie_it->key << "\n!=\n"
-                      << map_it->first.size() << "\n"
-                      << map_it->first << "\n";
-        }
-        if (trie_it->value != map_it->second) {
-            std::cout << trie_it->value << " != " << map_it->second << "\n";
-        }
-        if (trie_it->key != map_it->first || trie_it->value != map_it->second) {
-            ofs.close();
+    {
+        auto trie_it = trie.begin();
+        auto const trie_end = trie.end();
+        auto map_it = map.begin();
+        for (; trie_it != trie_end; ++trie_it, ++map_it) {
+            if (trie_it->key != map_it->first) {
+                std::cout << trie_it->key.size() << "\n"
+                          << trie_it->key << "\n!=\n"
+                          << map_it->first.size() << "\n"
+                          << map_it->first << "\n";
+            }
+            if (trie_it->value != map_it->second) {
+                std::cout << trie_it->value << " != " << map_it->second << "\n";
+            }
+            if (trie_it->key != map_it->first ||
+                trie_it->value != map_it->second) {
 #if ENABLE_DUMP
-            dump(std::cout, trie);
+                dump(std::cout, trie);
 #endif
+            }
+            assert(trie_it->key == map_it->first);
+            assert(trie_it->value == map_it->second);
         }
-        assert(trie_it->key == map_it->first);
-        assert(trie_it->value == map_it->second);
+        assert(map_it == map.end());
     }
-    assert(map_it == map.end());
+
+    {
+        auto trie_it = trie.rbegin();
+        auto const trie_end = trie.rend();
+        auto map_it = map.rbegin();
+        for (; trie_it != trie_end; ++trie_it, ++map_it) {
+            if (trie_it->key != map_it->first) {
+                std::cout << trie_it->key.size() << "\n"
+                          << trie_it->key << "\n!=\n"
+                          << map_it->first.size() << "\n"
+                          << map_it->first << "\n";
+            }
+            if (trie_it->value != map_it->second) {
+                std::cout << trie_it->value << " != " << map_it->second << "\n";
+            }
+            if (trie_it->key != map_it->first ||
+                trie_it->value != map_it->second) {
+#if ENABLE_DUMP
+                dump(std::cout, trie);
+#endif
+            }
+            assert(trie_it->key == map_it->first);
+            assert(trie_it->value == map_it->second);
+        }
+        assert(map_it == map.rend());
+    }
 }
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const * data, size_t size)
@@ -86,7 +119,12 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const * data, size_t size)
         size -= sizeof(action_t);
         if (0 <= action.op_ && action.op_ < action_t::num_ops &&
             size < INT_MAX) {
-            execute(action, boost::text::string_view((char const *)data, size));
+            auto str = boost::text::string_view((char const *)data, size);
+            if (std::all_of(str.begin(), str.end(), [](char c) {
+                    return std::isprint(c);
+                })) {
+                execute(action, str);
+            }
         }
     }
     return 0;
