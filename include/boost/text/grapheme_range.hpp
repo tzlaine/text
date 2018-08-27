f5066ed9a01bac577e0886c9b7b48d4042855f11
Zach Laine <whatwasthataddress@gmail.com>
2018-08-26 22:05:33 -0500

    Add grapheme_range.

---
+++ include/boost/text/grapheme_range.hpp
@@ -0,0 +1,34 @@
+#ifndef BOOST_TEXT_TEXT_GRAPHEME_RANGE_HPP
+#define BOOST_TEXT_TEXT_GRAPHEME_RANGE_HPP
+
+#include <boost/text/grapheme_iterator.hpp>
+
+
+namespace boost { namespace text {
+
+    /** A range of graphemes. */
+    template<typename CPIter>
+    struct grapheme_range
+    {
+        using iterator = grapheme_iterator<CPIter>;
+
+        grapheme_range() : first_(), last_() {}
+
+        grapheme_range(CPIter first, CPIter last) :
+            first_(first, first, last),
+            last_(first, last, last)
+        {}
+
+        bool empty() const noexcept { return first_ == last_; }
+
+        iterator begin() const noexcept { return first_; }
+        iterator end() const noexcept { return last_; }
+
+    private:
+        iterator first_;
+        iterator last_;
+    };
+
+}}
+
+#endif
