c016b969a45a2b51a9f25b1584c4c768d4621328
Zach Laine <whatwasthataddress@gmail.com>
2018-09-01 10:17:23 -0500

    Add unicode_version().

---
+++ include/boost/text/unicode_version.hpp
@@ -0,0 +1,15 @@
+#ifndef BOOST_TEXT_UNICODE_VERSION_HPP
+#define BOOST_TEXT_UNICODE_VERSION_HPP
+
+#include <boost/text/config.hpp>
+
+
+namespace boost { namespace text {
+
+    /** Returns the major number for the version of Unicode implemented by
+        this library. */
+    BOOST_TEXT_DECL int unicode_version();
+
+}}
+
+#endif
---
+++ src/unicode_version.cpp
@@ -0,0 +1,8 @@
+#include <boost/text/unicode_version.hpp>
+
+
+namespace boost { namespace text {
+
+    int unicode_version() { return 10; }
+
+}}
