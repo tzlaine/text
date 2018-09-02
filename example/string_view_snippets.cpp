b50d50b3868b5c7296e9cf0fbc6af24faea8577c
Zach Laine <whatwasthataddress@gmail.com>
2018-09-02 17:21:03 -0500

    Copy editing and small doc fixes.

--- example/string_view_snippets.cpp
+++ example/string_view_snippets.cpp
@@ -4,6 +4,14 @@
 
 int main ()
 {
+//[ string_view_lotsa_constexpr
+constexpr boost::text::string_view tv_1 = "Compile-time value.";
+constexpr boost::text::string_view tv_2 = tv_1(0, 7); // Slice off the front.
+static_assert(tv_2 == "Compile");
+//]
+(void)tv_1;
+(void)tv_2;
+
 //[ string_view_literal
 using namespace boost::text::literals;
 boost::text::string_view tv = "UDLs are, literally, the best idea."_sv;
