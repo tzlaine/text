5d93769ddb3f5c1ca0883868c99cc96661fc0386
Zach Laine <whatwasthataddress@gmail.com>
2018-09-02 23:40:17 -0500

    Fix broken C++11 build.

--- example/string_view_snippets.cpp
+++ example/string_view_snippets.cpp
@@ -5,12 +5,16 @@
 int main ()
 {
 //[ string_view_lotsa_constexpr
+#ifndef BOOST_TEXT_NO_CXX14_CONSTEXPR // Requires C++14 constexpr support.
 constexpr boost::text::string_view tv_1 = "Compile-time value.";
-constexpr boost::text::string_view tv_2 = tv_1(0, 7); // Slice off the front.
-static_assert(tv_2 == "Compile");
+constexpr boost::text::string_view tv_2 = tv_1(0, 7); // Slice off a prefix.
+static_assert(tv_2 == "Compile", "Something is really wrong.");
+#endif
 //]
+#ifndef BOOST_TEXT_NO_CXX14_CONSTEXPR
 (void)tv_1;
 (void)tv_2;
+#endif
 
 //[ string_view_literal
 using namespace boost::text::literals;
