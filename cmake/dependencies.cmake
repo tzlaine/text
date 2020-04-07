1d0e1324b281cdcbac094ba97de2aa2ba812bd44
Zach Laine <whatwasthataddress@gmail.com>
2020-04-07 17:44:29 -0500

    Cruft removal.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -71,29 +71,6 @@ else ()
   add_dependencies(boost boost_clone)
   target_include_directories(boost INTERFACE ${CMAKE_BINARY_DIR}/boost_root)
 endif ()
-#set(Boost_USE_STATIC_LIBS ON)
-#find_package(Boost 1.64.0 COMPONENTS ${boost_components})
-#if (Boost_INCLUDE_DIR)
-#  add_library(boost INTERFACE)
-#  target_include_directories(boost INTERFACE ${Boost_INCLUDE_DIR})
-#else ()
-#  message("-- Boost was not found; attempting to download it if we haven't already...")
-#  include(ExternalProject)
-#  ExternalProject_Add(install-Boost
-#    PREFIX ${CMAKE_BINARY_DIR}/dependencies/boost_1_64_0
-#    URL https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
-#    CONFIGURE_COMMAND ""
-#    BUILD_COMMAND ""
-#    INSTALL_COMMAND ""
-#    LOG_DOWNLOAD ON
-#  )
-#
-#  ExternalProject_Get_Property(install-Boost SOURCE_DIR)
-#  add_library(boost INTERFACE)
-#  target_include_directories(boost INTERFACE ${SOURCE_DIR})
-#  add_dependencies(boost install-Boost)
-#  unset(SOURCE_DIR)
-#endif ()
 
 
 ###############################################################################
