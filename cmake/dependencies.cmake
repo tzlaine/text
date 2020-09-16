bd8577d3f0d002ef363b8208fa45083e4db11dec
Zach Laine <whatwasthataddress@gmail.com>
2020-09-15 20:20:00 -0500

    Remove the boost target as a direct CMake dependency of text.
    Partially addresses #184.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -12,64 +12,72 @@ if (Boost_INCLUDE_DIR)
   add_library(boost INTERFACE)
   target_include_directories(boost INTERFACE ${Boost_INCLUDE_DIR})
 else ()
-  if (NOT BOOST_BRANCH)
-    set(BOOST_BRANCH master)
+  if (NOT EXISTS ${CMAKE_BINARY_DIR}/boost_root)
+    if (NOT BOOST_BRANCH)
+      set(BOOST_BRANCH master)
+    endif()
+    message("-- Boost was not found; it will be cloned locally from ${BOOST_BRANCH}.")
+    execute_process(
+      COMMAND
+      git clone --depth 100 -b ${BOOST_BRANCH}
+        https://github.com/boostorg/boost.git boost_root
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    if (MSVC)
+      set(bootstrap_cmd ./bootstrap.bat)
+    else()
+      set(bootstrap_cmd ./bootstrap.sh)
+    endif()
+    execute_process(
+      COMMAND git submodule init libs/test
+      COMMAND git submodule init libs/algorithm
+      COMMAND git submodule init libs/align
+      COMMAND git submodule init libs/array
+      COMMAND git submodule init libs/bind
+      COMMAND git submodule init libs/concept_check
+      COMMAND git submodule init libs/container
+      COMMAND git submodule init libs/container_hash
+      COMMAND git submodule init libs/exception
+      COMMAND git submodule init libs/filesystem
+      COMMAND git submodule init libs/function
+      COMMAND git submodule init libs/integer
+      COMMAND git submodule init libs/intrusive
+      COMMAND git submodule init libs/io
+      COMMAND git submodule init libs/iterator
+      COMMAND git submodule init libs/move
+      COMMAND git submodule init libs/mpl
+      COMMAND git submodule init libs/numeric
+      COMMAND git submodule init libs/optional
+      COMMAND git submodule init libs/preprocessor
+      COMMAND git submodule init libs/range
+      COMMAND git submodule init libs/smart_ptr
+      COMMAND git submodule init libs/static_assert
+      COMMAND git submodule init libs/system
+      COMMAND git submodule init libs/type_index
+      COMMAND git submodule init libs/type_traits
+      COMMAND git submodule init libs/throw_exception
+      COMMAND git submodule init libs/utility
+      COMMAND git submodule init libs/assert
+      COMMAND git submodule init libs/config
+      COMMAND git submodule init libs/core
+      COMMAND git submodule init libs/predef
+      COMMAND git submodule init libs/detail
+      COMMAND git submodule init tools/build
+      COMMAND git submodule init libs/headers
+      COMMAND git submodule init tools/boost_install
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root)
+    execute_process(
+      COMMAND git submodule update --jobs 3
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root)
+    execute_process(
+      COMMAND ${bootstrap_cmd}
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root)
+    execute_process(
+      COMMAND ./b2 headers
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root)
   endif()
-  message("-- Boost was not found; it will be cloned locally from ${BOOST_BRANCH}.")
-  add_custom_target(
-    boost_root_clone
-    git clone --depth 100 -b ${BOOST_BRANCH}
-      https://github.com/boostorg/boost.git boost_root
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-  if (MSVC)
-    set(bootstrap_cmd ./bootstrap.bat)
-  else()
-    set(bootstrap_cmd ./bootstrap.sh)
-  endif()
-  add_custom_target(
-    boost_clone
-    COMMAND git submodule init libs/test
-    COMMAND git submodule init libs/algorithm
-    COMMAND git submodule init libs/align
-    COMMAND git submodule init libs/array
-    COMMAND git submodule init libs/bind
-    COMMAND git submodule init libs/concept_check
-    COMMAND git submodule init libs/container
-    COMMAND git submodule init libs/container_hash
-    COMMAND git submodule init libs/exception
-    COMMAND git submodule init libs/function
-    COMMAND git submodule init libs/integer
-    COMMAND git submodule init libs/intrusive
-    COMMAND git submodule init libs/io
-    COMMAND git submodule init libs/iterator
-    COMMAND git submodule init libs/move
-    COMMAND git submodule init libs/mpl
-    COMMAND git submodule init libs/numeric
-    COMMAND git submodule init libs/optional
-    COMMAND git submodule init libs/preprocessor
-    COMMAND git submodule init libs/range
-    COMMAND git submodule init libs/smart_ptr
-    COMMAND git submodule init libs/static_assert
-    COMMAND git submodule init libs/type_index
-    COMMAND git submodule init libs/type_traits
-    COMMAND git submodule init libs/throw_exception
-    COMMAND git submodule init libs/utility
-    COMMAND git submodule init libs/assert
-    COMMAND git submodule init libs/config
-    COMMAND git submodule init libs/core
-    COMMAND git submodule init libs/predef
-    COMMAND git submodule init libs/detail
-    COMMAND git submodule init tools/build
-    COMMAND git submodule init libs/headers
-    COMMAND git submodule init tools/boost_install
-    COMMAND git submodule update --jobs 3
-    COMMAND ${bootstrap_cmd}
-    COMMAND ./b2 headers
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/boost_root
-    DEPENDS boost_root_clone)
   add_library(boost INTERFACE)
-  add_dependencies(boost boost_clone)
   target_include_directories(boost INTERFACE ${CMAKE_BINARY_DIR}/boost_root)
+  set(Boost_INCLUDE_DIR ${CMAKE_BINARY_DIR}/boost_root)
 endif ()
 
 
